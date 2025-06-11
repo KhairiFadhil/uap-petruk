cd505ec9924f5ebddd2eebbd31410e60c09d5148
Khalishazahwa15 <khalishazahwa360@gmail.com>
2025-06-11 19:43:16 +0700

    Update stock_data.cpp

--- src/stock_data.cpp
+++ src/stock_data.cpp
@@ -1,9 +1,13 @@
 #include "stock_data.hpp"
 #include <algorithm>
 #include <numeric>
+#include <cmath>
 
 void StockDataProcessor::preprocessData(std::vector<StockPoint>& data) {
-  
+    // Konversi tanggal ke nilai numerk (hari ke-n)
+  for (size_t i = 0; i < data.size(); i++){
+    data[i].dayIndex = static_cast<int>(i);
+  }
 }
 
 
@@ -22,8 +26,32 @@ StockDataProcessor::DataSummary StockDataProcessor::getSummary(const std::vector
     if (data.empty()) {
         return summary;
     }
-    
+
+    double sumClose = 0.0;
+    long sumVolume = 0;
+
+    summary.minClose = data[0].close;
+    summary.maxClose = data[0].close;
+    summary.minVolume = data[0].volume;
+    summary.maxVolume = data[0].volume;
+
+    for (const auto& point : data){
+      sumClose += point.close;
+      sumVolume += point.volume;
+
+      if (point.close < summary.minClose) summary.minClose = point.close;
+      if (point.close > summary.maxClose) summary.maxClose = point.close;
+
+      if (point.volume < summary.minVolume) summary.minVolume = point.volume;
+      if (point.volume > summary.maxVolume) summary.maxVolume = point.volume;
+
+    }
+
+  summary.avgClose = sumClose / data.size();
+  summary.avgVolume = static_cast<double>(sumVolume) / data.size();
+  summary.totalDays = data.size();
     // Hitung min, max, rata-rata harga
+      
     // Hitung min, max, rata-rata volume
     // Set total hari
     
